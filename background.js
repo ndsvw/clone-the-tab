062a7a216b0c95cea2d583db2eeb5112600c89ee
Alex Lübeck <omega.zero@outlook.com>
2018-09-02 22:01:58 +0200

    using the duplicate function instead of create

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 let duplicate = async () => {
   let cur = await browser.tabs.query({ active: true });
-  await browser.tabs.create({ url: cur[0].url });
+  await browser.tabs.duplicate(cur[0].id);
 }
 
 browser.browserAction.onClicked.addListener(async () => {
