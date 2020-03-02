2b24d78cd151968711d321c990da188368222718
Alex Lübeck <ndsvw@gmx.de>
2020-03-02 21:33:15 +0100

    added semicolons

--- background.js
+++ background.js
@@ -2,14 +2,14 @@ let duplicate = async () => {
   let curWindow = await browser.windows.getCurrent({populate: true});
   let curTab = (await browser.tabs.query({ active: true, windowId: curWindow.id}))[0];
   await browser.tabs.duplicate(curTab.id);
-}
+};
 
 browser.browserAction.onClicked.addListener(async () => {
-  await duplicate()
+  await duplicate();
 });
 
 browser.commands.onCommand.addListener(async command => {
   if (command == "execute") {
-    await duplicate()
+    await duplicate();
   }
 });
